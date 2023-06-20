d193facb6c64764f09f9b3d0577e5b2ec0c7e154
Iya_Campo <ilyannacampo@gmail.com>
2023-06-20 15:39:49 +0800

    UX improvements

--- src/components/PokeMart/ShopItem.tsx
+++ src/components/PokeMart/ShopItem.tsx
@@ -5,12 +5,12 @@ import { checkItems } from '@/utils/Utils';
 
 function ShopItem(props: any) {
   return (
-    <Box key={props.item.id} component='div' display='flex' alignItems='center' flexWrap='wrap' py={1}>
+    <Box key={props.item.id} component='div' display='flex' alignItems='center' flexWrap='wrap' py={1} rowGap={1}>
       <Typography component='span' display='block' width={200}>
         {props.item.name}
       </Typography>
       <Box component='div' pr={!props.isMobile ? 6 : 2}>
-        <Button variant='outlined' size='small' disabled={props.item.inCart <= 0} onClick={() => props.removeFromCart(props.item)}>
+        <Button variant='outlined' disabled={props.item.inCart <= 0} sx={{ minWidth: 5 }} onClick={() => props.removeFromCart(props.item)}>
           -
         </Button>
         <Typography
@@ -22,13 +22,13 @@ function ShopItem(props: any) {
         >
           {props.item.inCart}
         </Typography>
-        <Button variant='outlined' size='small' disabled={props.item.inCart >= 99} onClick={() => props.addToCart(props.item)}>
+        <Button variant='outlined' disabled={props.item.inCart >= 99} sx={{ minWidth: 5 }} onClick={() => props.addToCart(props.item)}>
           +
         </Button>
       </Box>
       <Box component='div' display='flex'>
-        <Typography component='span' width='70px' pr={1}>
-          Owned:
+        <Typography component='span' width={!props.isMobile ? '70px' : 'fit-content'} pr={1}>
+          In Bag:
         </Typography>
         <Typography component='span'>{checkItems(props.playerItems, props.item.name, 'quantity')}</Typography>
       </Box>
