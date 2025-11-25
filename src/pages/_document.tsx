c96f99cb0a354ce2e5b6368af6661f77c05e3452
iya-campo <ilyannacampo@gmail.com>
2025-11-25 20:23:38 +0800

    Fix: Images broken in GitHub pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <link rel='poke-app icon' href='/images/poke-app.png' />
+      <link rel='poke-app icon' href='/poke-app/images/poke-app.png' />
       <title>Poke App v1</title>
       <Head />
       <body>
